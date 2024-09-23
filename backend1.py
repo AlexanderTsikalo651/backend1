f8e52ca67bfecf048c294e8f5c417a45b2d415b5
AlexanderTsikalo651 <tsikalo651@mail.ru>
2024-09-23 22:14:09 +0300

    Backend version 2.0

--- backend1.py
+++ backend1.py
@@ -1,13 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/data', methods=['POST'])
 def save_data():
     text = request.form['text']
-    with open('../data.txt', 'a') as file:
+    with open('data.txt', 'a') as file:
         file.write(text + '\n')
     return 'Data received and saved.'
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    with open('data.txt', 'r') as file:
+        data = file.readlines()
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
